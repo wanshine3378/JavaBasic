8bdde78b7b041c6c6095667e0332a2e1360c09f9
TorresLin <38996306+TorresLin@users.noreply.github.com>
2022-02-12 13:37:21 +0800

    sort 調整
    insertion sort 補完

--- src/com/example/demo/sort/InsertionSort.java
+++ src/com/example/demo/sort/InsertionSort.java
@@ -9,10 +9,14 @@ public class InsertionSort {
         List<Integer> sorted = new ArrayList<Integer>(array.length);
         for(int i=0; i<array.length;++i)
         {
-            int n = array[i];
+            int n = array[i];//asdfasdf
             int index = sorted.size() - 1;
             while(index >= 0 && sorted.get(index)>n)
                 --index;
+            sorted.add(index+1, n);
+        }
+        for(int i=0; i<array.length;i++) {
+        	array[i] = sorted.get(i);
         }
     }
 }
